4053aba0dffaf64ed2196b6c72d8790f2e767311
Ryan Bourgeois <bluedragonx@gmail.com>
2013-03-31 02:30:45 -0700

    Turn repo into a setuptools package.

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+requires = []
+
+setup(
+    name='qbin',
+    version=version,
+    description="Gentoo Binary Package Manager",
+    long_description="qbin is a system for managing Gentoo binary packages.",
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[],
+    keywords='',
+    author='Ryan Bourgeois',
+    author_email='bluedragonx@gmail.com',
+    url='https://github.com/BlueDragonX/qbin',
+    license='GPLv2',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    tests_require=requires,
+    test_suite="",
+    entry_points="",
+)
